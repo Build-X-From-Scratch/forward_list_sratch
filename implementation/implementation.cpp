3bae1283b0d67803b01901f83552e1fd72320c82
Muh Yusuf <yusufmuhyusuh@gmail.com>
2025-09-12 15:50:13 +0700

    feat: menyelesaikan method insert_after

--- implementation/implementation.cpp
+++ implementation/implementation.cpp
@@ -9,8 +9,14 @@ int main(){
     fl.push_front(1);
     fl.push_front(2);
     fl.push_front(3);
+    std::cout << std::endl;
+    //=========== Menggunakan deferencing==============
     for(auto it = fl.begin(); it != fl.end(); ++it) {
-        std::cout << *it << " " << std::endl;
+        std::cout << *it << " ";
+    }
+    //================ Tanpa deferencing ===============
+    for(auto it = fl.begin();it != fl.end();++it){
+        std::cout << it << " ";
     }
     std::cin.get();
     return 0;
