272a08a9b674515f3c4ab2bcb8a2ea7a387a7b38
Muh Yusuf <yusufmuhyusuh@gmail.com>
2025-09-16 16:33:35 +0700

    feat: menambahkan unit testing dan overload func

--- implementation/implementation.cpp
+++ implementation/implementation.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "../header/forward_list.hpp"
 int main(){
     //initializer list
@@ -23,10 +24,31 @@ int main(){
         std::cout << it << " ";
     }
     std::cout << std::endl;
-    //================insert_after=======================
-    fl.insert_after(fl.before_begin(),1);
+    //================insert_after 1=======================
+    int x = 2;
+    fl.insert_after(fl.before_begin(),x);
     std::cout << "Proses insert_after 1" << std::endl;
     fl.print_all(fl.begin(),fl.end());
-    std::cin.get();
+    //================insert after 2======================== 
+    forward_lists<int>fl1 = {1,2,3};
+    std::cout << "insert after 2" << std::endl;
+    fl1.insert_after(fl1.begin(),3,10);
+    fl1.print_all(fl1.begin(),fl1.end()); //1 10 10 10 2 3
+    //===============insert after 3=================================
+    std::vector<int>v = {10,20,30};
+    std::cout << "insert after 3" << std::endl;
+    forward_lists<int>fl2 = {5,6,7};
+    fl2.insert_after(fl2.begin(),v.begin(),v.end());
+    fl2.print_all(fl2.begin(),fl2.end());
+    //===============insert after 4=================================
+    forward_lists<int>fl3 = {8,9,10};
+    forward_lists<int>fwl = {50,60,40};
+    fl3.insert_after(fl3.begin(),fwl.begin(),fwl.end());
+    fl3.print_all(fl3.begin(), fl3.end()); //8 50 60 40 9 10
+    //===============Proses Erasing===================================
+    std::cout << "Pop Front" << std::endl;
+    forward_lists<int>flst = {1,2,3};
+    flst.print_all(flst.begin(),flst.end()); //2 3
+    //std::cin.get();
     return 0;
 }   
