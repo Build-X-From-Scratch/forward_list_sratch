4351bba5d6f84010e27a9cf0fc1369df6c1dd2c0
Muh Yusuf <yusufmuhyusuh@gmail.com>
2025-10-07 20:08:50 +0700

    feat: update benchmark

--- benchmarking/benchmarking.cpp
+++ benchmarking/benchmarking.cpp
@@ -43,6 +43,42 @@ static void BM_stl_single_push(benchmark::State& state){
         benchmark::DoNotOptimize(list);
         state.SetComplexityN(state.iterations());
     }
+}   
+BENCHMARK(BM_stl_single_push)->Arg(1000)->Arg(100000)->Complexity();  
+
+static void BM_My_Insert_afterI(benchmark::State& state){
+    for(auto _ : state){
+        forward_lists<int>list;
+        std::vector<int>v;
+        for(int i = 0;i < state.range(0);i++){
+            list.push_front(i);
+        }
+        auto pos = list.begin();
+        for(int i = 0;i < 1000;i++){
+            list.insert_after(pos,i);
+        }
+        state.SetComplexityN(state.iterations());
+        benchmark::DoNotOptimize(list);
+    }
 }
-BENCHMARK(BM_stl_single_push)->Arg(1000)->Arg(100000)->Complexity();
+BENCHMARK(BM_My_Insert_afterI)->Arg(1000)->Arg(10000)->Arg(50000)->Complexity();
+
+BENCHMARK(BM_stl_single_push)->Arg(1000)->Arg(100000)->Complexity();  
+
+static void BM_stl_Insert_afterI(benchmark::State& state){
+    for(auto _ : state){
+        std::forward_list<int>list;
+        std::vector<int>v;
+        for(int i = 0;i < state.range(0);i++){
+            list.push_front(i);
+        }
+        auto pos = list.begin();
+        for(int i = 0;i < 1000;i++){
+            list.insert_after(pos,i);
+        }
+        state.SetComplexityN(state.iterations());
+        benchmark::DoNotOptimize(list);
+    }
+}
+BENCHMARK(BM_stl_Insert_afterI)->Arg(1000)->Arg(10000)->Arg(50000)->Complexity();
 BENCHMARK_MAIN();
